2ae5879bbbfee8ff7ef870f97191dfa883b5c1ba
juhodev <jooo.dev@gmail.com>
2019-12-28 14:57:47 +0200

    Add a message queue to Writer
    I believe this will fix client crashing when big files are being sent. A big file would get sent and while that's being sent I write a ping to the client and it writes it to the file.
    
    Now pings don't get sent at all and messages are queued while a file is being sent.

--- src/main/java/dev/juho/ffmpegrender/server/client/Writer.java
+++ src/main/java/dev/juho/ffmpegrender/server/client/Writer.java
@@ -7,25 +7,38 @@ import dev.juho.ffmpegrender.utils.Logger;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Writer {
 
 	private OutputStream os;
 
+	//	Queue messages so they won't be sent while sending big files
+	private Queue<Message> messageQueue;
+	private boolean writingFile;
+
 	public Writer(OutputStream os) {
 		this.os = os;
+		this.messageQueue = new LinkedList<>();
+		this.writingFile = false;
 	}
 
 	public void write(Message message) {
-		Logger.getInstance().log(Logger.DEBUG, "Writing a message " + message.getData().toString());
-		try {
-			os.write(message.toByteArray());
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (!writingFile) {
+			Logger.getInstance().log(Logger.DEBUG, "Writing a message " + message.getData().toString());
+			try {
+				os.write(message.toByteArray());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			messageQueue.add(message);
 		}
 	}
 
 	public void write(File file) {
+		writingFile = true;
 		ByteBuffer infoBuffer = createFileInfoBuffer(file.length(), file.getName());
 		Logger.getInstance().log(Logger.DEBUG, "sending " + file.getAbsolutePath());
 		Logger.getInstance().createProgressbar("Sending file", file.length());
@@ -47,13 +60,17 @@ public class Writer {
 
 			Logger.getInstance().removeProgressbar("Sending file");
 			bis.close();
+			writingFile = false;
+			flushQueue();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void sendPing() throws IOException {
-		os.write(MessageIdentifier.PING);
+		if (!writingFile) {
+			os.write(MessageIdentifier.PING);
+		}
 	}
 
 	private ByteBuffer createFileInfoBuffer(long fileLength, String name) {
@@ -65,6 +82,13 @@ public class Writer {
 		infoBuffer.put(name.getBytes());
 		infoBuffer.putLong(fileLength);
 		return infoBuffer;
+	}
+
+	private void flushQueue() {
+		while (!messageQueue.isEmpty()) {
+			Message msg = messageQueue.poll();
+			write(msg);
+		}
 	}
 
 	public void close() throws IOException {
